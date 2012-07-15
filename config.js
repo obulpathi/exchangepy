e49327a0e0cb77e834d2e0add243bce14a8f304c
Andrew Vorobyov <vorandrew@gmail.com>
2012-07-15 16:17:06 +0000

    Basic auth + need to update sha1 function on client side

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {};
+
+config.pg = {};
+
+config.pg.username = 'exchange';
+config.pg.password = 'qweqwe';
+config.pg.host = 'localhost';
+config.pg.db = 'exchange';
+
+module.exports = config;
