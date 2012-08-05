43233604bac0c2ec9e53c741be72977e638423de
Andrew Vorobyov <vorandrew@gmail.com>
2012-08-05 17:25:47 +0000

    Symbols

--- sql/init.sql
+++ sql/init.sql
@@ -3,12 +3,12 @@ TRUNCATE users CASCADE;
 TRUNCATE matched CASCADE;
 TRUNCATE orders_limit CASCADE;
 TRUNCATE balances CASCADE;
+TRUNCATE users CASCADE;
 
-INSERT INTO symbols VALUES ( 1 , 'BTC', 'BTC', TRUE, FALSE, 1, 1, 1, NULL);
-INSERT INTO symbols VALUES ( 2 , 'USD', 'USD', TRUE, FALSE, 6.1, 6.2, 6.1, now());
-INSERT INTO symbols VALUES ( 3 , 'EUR', 'EUR', TRUE, FALSE, 5.1, 5.2, 5.1, now());
-INSERT INTO symbols VALUES ( 4 , 'GBP', 'GBP', TRUE, FALSE, 4.2, 4.3, 4.25, now());
-INSERT INTO symbols VALUES ( 5 , 'XAU', 'XAU', FALSE, TRUE, 270.49, 271.49, 270.9, now());
+INSERT INTO symbols VALUES (3, 'EUR', 'EUR', 0.08524, 0.08224, 0.08524, NULL, 2);
+INSERT INTO symbols VALUES (2, 'USD', 'USD', 0.09524, 0.09434, 0.09524, NULL, 2);
+INSERT INTO symbols VALUES (1, 'BTC', 'BTC', 1.00000, 1.00000, 1.00000, NULL, 2);
+INSERT INTO symbols VALUES (4, 'XAU', 'XAU', 152.38095, 153.38095, 152.38095, NULL, 10);
 
 INSERT INTO users VALUES (1, 'vorandrew@gmail.com', 'qweqwe', '13:28:51.896484', 1);
 INSERT INTO users VALUES (2, 'voran333@mail.ru', 'qweqwe', '13:30:56.971909', 2);
