f7ceea1b59a39de7b8a81540693202581d91b4ac
Andrew Vorobyov <vorandrew@gmail.com>
2012-08-06 15:23:16 +0000

    Order tests

--- test/test.js
+++ test/test.js
@@ -3,10 +3,10 @@ var Account = require('../lib/account.js');
 
 describe('Account', function ()
 {
+	var account = new Account();
+
 	describe('Login', function ()
 	{
-		var account = new Account();
-
 		var token = {
 			id        : 3,
 			timestamp : 1344262673623,
@@ -30,5 +30,29 @@ describe('Account', function ()
 				}
 			});
 		});
-	})
-})
+	});
+
+	var payload = {
+		symbol   : 'usd',
+		expire   : new Date( new Date().getTime() + 5000000),
+		buy_sell : true,
+		price    : 0.05455,
+		type     : 'gtc',
+		amount   : 10
+	};
+
+	describe('Orders', function ()
+	{
+		it('gtc order', function (done)
+		{
+			account.new_order(payload, done);
+		});
+
+		it('stop order', function (done)
+		{
+			payload.type = 'stop';
+			account.new_order(payload, done);
+		});
+
+	});
+});
